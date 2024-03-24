a76c6b4e039b5c7739377f68cd33bf599eeb101a
danielolasupo02 <danielolasupo02@gmail.com>
2024-03-24 23:48:23 +0100

    complete video 175 from spring mvc

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.example.thymeleafdemo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     // need a controller method to show initial form
     @RequestMapping("/showForm")
     public String showForm() {
@@ -35,6 +40,29 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // Your controller logic
+        String yourVariable = "My name is "+ theName;
+        logger.info("Your variable value: {}", yourVariable);
+
+
+        // create the message
+        String result = "Hey my friend from V3! " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
