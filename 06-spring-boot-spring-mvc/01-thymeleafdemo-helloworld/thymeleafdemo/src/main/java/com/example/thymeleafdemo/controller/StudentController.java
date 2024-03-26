265439b7469cca715c5c5e6fe42f4250a249bb17
danielolasupo02 <danielolasupo02@gmail.com>
2024-03-26 23:59:02 +0100

    complete spring mvc video 190

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,25 @@
 package com.example.thymeleafdemo.controller;
 
 import com.example.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    List<String> countries;
+
+    @Value("${languages}")
+    List<String> languages;
+
+    @Value("${operatingSystems}")
+    List<String> operatingSystems;
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -17,6 +28,12 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
+        // add the list of languages to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
