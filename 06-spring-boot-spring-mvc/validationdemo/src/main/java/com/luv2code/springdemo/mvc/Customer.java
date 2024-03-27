0714f5f35d3f5ec63fb47a411c03e399bca46093
danielolasupo02 <danielolasupo02@gmail.com>
2024-03-27 21:26:19 +0100

    complete spring mvc entire section video @215

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,9 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -14,15 +12,38 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
+    @NotNull(message="is required")
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
-    public int getFreePasses() {
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    @CourseCode(value="TOPS", message="must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
